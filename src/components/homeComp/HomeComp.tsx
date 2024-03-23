4a815ecc4eba184196054937c1825fbc5654ca42
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-03-23 13:23:30 -0300

    Clian HomeComp

--- src/components/homeComp/HomeComp.tsx
+++ src/components/homeComp/HomeComp.tsx
@@ -3,23 +3,8 @@ import styles from './_HomeComp.module.scss';
 
 const HomeComp = () => {
 
-
   return (
     <div className={styles.container}>
-      {/* <div className={styles.slider}>
-        {images.map((image, index) => (
-          <div
-          className={`${styles.slide} ${index === currentSlide ? styles.active : ''}`}
-            key={index}
-          >
-            <img
-              src={image}
-              alt=""
-              className={styles.img}
-              />
-          </div>
-        ))}
-      </div> */}
       <div className={styles.div}>
         <h1>HOME COMPONENT</h1>
       </div>
