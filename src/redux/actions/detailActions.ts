8e0bdf803703cacec6ff2b87936fddc1b760c42b
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-08-02 18:09:52 -0300

    detail

--- src/redux/actions/detailActions.ts
+++ src/redux/actions/detailActions.ts
@@ -1,29 +1,48 @@
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+
+
+// console.log('en detailActions')
+
+// export const getDetail: any = createAsyncThunk(
+//   'GetDetail', 
+//   async (_, thunkAPI) => {
+//     try {
+//       console.log('en en try');
+
+//       // Definir el cuerpo de la solicitud
+//       // const requestBody = {
+//       //   "attributes": [],
+//       //   "category": 0
+//       // };
+
+//       // Realizar la solicitud POST con el cuerpo JSON
+//       const response = await axios.get<any>('/GetAdvertising/advId=24');
+
+//       console.log('response.data en getDetail: ', response.data);
+//       const data = response.data;
+//       return data;
+//     } catch (error: any) {
+//       console.log('en catch');
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+
+
+// redux/actions/detailActions.ts
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-
-console.log('en detailActions')
-
-export const getDetail: any = createAsyncThunk(
+export const getDetail = createAsyncThunk(
   'GetDetail', 
-  async (_, thunkAPI) => {
+  async (offerId: string, thunkAPI) => {
     try {
-      console.log('en en try');
-
-      // Definir el cuerpo de la solicitud
-      const requestBody = {
-        "attributes": [],
-        "category": 0
-      };
-
-      // Realizar la solicitud POST con el cuerpo JSON
-      const response = await axios.post<any>('/GetDetail', requestBody);
-
-      console.log('response.data en getAllPremiumAdv: ', response.data);
+      const response = await axios.get<any>(`/GetAdvertising/advId=${offerId}`);
       const data = response.data;
       return data;
     } catch (error: any) {
-      console.log('en catch');
       return thunkAPI.rejectWithValue(error.message);
     }
   }
