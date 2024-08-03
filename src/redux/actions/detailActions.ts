efb999d53ee95938e0405bc964897b516a974782
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-08-03 11:14:44 -0300

    OfferDetailcomp

--- src/redux/actions/detailActions.ts
+++ src/redux/actions/detailActions.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const getDetail = createAsyncThunk(
+export const getDetail: any = createAsyncThunk(
   'GetDetail', 
   async (offerId: string, thunkAPI) => {
     try {
