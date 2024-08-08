65f55f2d27e98822997e60bef213ae0cecdd4f0d
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-08-08 12:49:42 -0300

    styles

--- src/components/offerComps/offerListItemComp/OfferListItemComp.tsx
+++ src/components/offerComps/offerListItemComp/OfferListItemComp.tsx
@@ -5,22 +5,18 @@ import TagComp from '../../tagComp/TagComp';
 
 const OfferListItemComp = (data: any) => {
 
-  const img_style = {
-    height: '300px', // Altura fija deseada
-    width: '100%', // Ancho completo
-    backgroundImage: `url(${data.data.images[0].path})`, // URL de la imagen de fondo
-    backgroundSize: 'cover', // Cubrir el contenedor
-    backgroundPosition: 'center', // Centrar la imagen de fondo
-  };
-
   return (
     <div className={styles.list_item_box}>
-      <img 
-        src={data.data.images[0].path}
-        alt="Imagen de la oferta"
-        className={styles.list_item_image}
-      />
-      {/* <div style={img_style} className={styles.list_item_image}/> */}
+      <div className={styles.list_item_left}>
+        <img 
+          src={data.data.images[0].path}
+          alt="Imagen de la oferta"
+          className={styles.list_item_image}
+        />
+        <div className={styles.list_item_bottom}>
+          <h6 className={styles.list_item_price}>${data.data.price}</h6>
+        </div>
+      </div>
       <div className={styles.list_item_details}>
         <div className={styles.list_item_top}>
           <h5 className={styles.list_item_title}>{data.data.title}</h5> 
@@ -37,9 +33,7 @@ const OfferListItemComp = (data: any) => {
             </div>
           ))}
         </div>
-        <div className={styles.list_item_bottom}>
-          <h6 className={styles.list_item_price}>${data.data.price}</h6>
-        </div>
+        
       </div>
     </div>
   );
